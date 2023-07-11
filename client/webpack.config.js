a78bd3371fe62dca08f54d3daab4c558d526957f
christopherflores9312 <christopherflores9312@gmail.com>
2023-07-10 23:04:41 -0700

    added webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,9 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
@@ -18,12 +15,57 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      // Generate HTML files for your entry points
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        chunks: ['main'],
+        filename: 'index.html',
+      }),
+      new HtmlWebpackPlugin({
+        template: './src/install.html',
+        chunks: ['install'],
+        filename: 'install.html',
+      }),
+      // Generate a manifest file for your Progressive Web App (PWA)
+      new WebpackPwaManifest({
+        name: 'My App',
+        short_name: 'App',
+        description: 'My awesome app',
+        background_color: '#ffffff',
+        theme_color: '#2196f3',
+        icons: [
+          {
+            src: path.resolve('src/assets/icon.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+          },
+        ],
+      }),
+      // Inject the service worker script into your HTML files
+      new InjectManifest({
+        swSrc: './src/js/service-worker.js',
+        swDest: 'service-worker.js',
+        exclude: [/\.map$/, /manifest\.json$/],
+      }),
     ],
-
     module: {
       rules: [
-        
+        // Add CSS loaders and Babel configuration if needed
+        // For example, if you want to process CSS and SCSS files:
+        {
+          test: /\.(css|scss)$/,
+          use: ['style-loader', 'css-loader', 'sass-loader'],
+        },
+        // Add Babel configuration for JavaScript files
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
       ],
     },
   };
